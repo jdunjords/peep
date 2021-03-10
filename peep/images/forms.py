2230e566fc5def20c025f41a6b033900a2589f23
cl2445 <cl2445@nau.edu>
2021-03-10 09:54:45 -0700

    integrate

--- peep/images/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from flask_wtf.file import FileField, FileAllowed
-from wtforms import StringField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired
-
-class ImageForm(FlaskForm):
-	picture = FileField('Select Image', validators=[FileAllowed(['jpg', 'png'])])
-	submit = SubmitField('Upload')
--- peep/posts/forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-# from flask_wtf.file import FileField, FileAllowed TODO need this for files in posts
-from wtforms import StringField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired
-
-class PostForm(FlaskForm):
-	title = StringField('Title', validators=[DataRequired()])
-	content = TextAreaField('Content', validators=[DataRequired()])
-	submit = SubmitField('Post')
