7aeff1b97bf946ad32dedb41c4dbeab784479d4d
Jonas Dunham Jordahl <jgd82@nau.edu>
2021-03-17 11:19:55 -0700

    db now autorefreshes before app is run

--- run.py
+++ run.py
@@ -2,5 +2,11 @@ from peep import create_app
 
 app = create_app()
 
+# Refresh db to get any newly added tables/columns
+from peep import db
+from peep.models import *
+app.app_context().push()
+db.create_all()
+
 if __name__ == '__main__':
 	app.run(debug=True)
