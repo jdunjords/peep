bc700e025466b4883a6b7815bae1e6bd5894d4cf
Jonas Dunham Jordahl <46722230+jdunjords@users.noreply.github.com>
2021-03-25 17:49:10 -0700

    Update routes.py
    still don't wanna talk about it

--- peep/users/routes.py
+++ peep/users/routes.py
@@ -35,17 +35,17 @@ def login():
 	form = LoginForm()
 	if form.validate_on_submit():
 		user = User.query.filter_by(email=form.email.data).first()
-                flash('user: ' + user.username, 'info')
+		flash('user: ' + user.username, 'info')
 		if user and bcrypt.check_password_hash(user.password, form.password.data):
 			login_user(user, remember=form.remember.data)
 			next_page = request.args.get('next')
-                        flash('login successful, trying to redirect...')
+			flash('login successful, trying to redirect...')
 			# TODO next page needs to be validated to avoid open redirect vulnerability
 			return redirect(next_page) if next_page else redirect(url_for('main.home'))
 		else:
 			flash('Login unsuccessful. Please check email and password.', 'danger')
         else:
-            flash('form failed to validate...', 'danger')
+		flash('form failed to validate...', 'danger')
 	return render_template("login.html", title="Login", form=form)
 
 
