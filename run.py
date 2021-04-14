86c5f6ad22d0112f94ed91652e525050165ec3b2
Jonas Dunham Jordahl <jgd82@nau.edu>
2021-04-14 14:46:28 -0700

    create a deleted user if we haven't done so yet

--- run.py
+++ run.py
@@ -3,10 +3,17 @@ from peep import create_app
 app = create_app()
 
 # Refresh db to get any newly added tables/columns
-from peep import db
+from peep import db, bcrypt
 from peep.models import *
 app.app_context().push()
 db.create_all()
+
+# create the deleted user if necessary
+if not User.query.filter_by(username="[deleted]").first():
+	deleted_hashed_password = bcrypt.generate_password_hash("deleteduser").decode('utf-8')
+	deleted_user = User(username="[deleted]", email="deleted@peep.com", password=deleted_hashed_password)
+	db.session.add(deleted_user)
+	db.session.commit()
 
 if __name__ == '__main__':
 	app.run(debug=True)
